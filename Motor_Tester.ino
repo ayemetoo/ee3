d7b9900a23518ffe456d9a0e596249ecfa04bc16
Amy Tu <tu.amy.a@gmail.com>
2017-08-24 13:59:24 -0700

    Add files via upload

---
+++ Motor_Tester.ino
@@ -0,0 +1,18 @@
+#define leftMotor 5
+#define rightMotor 3
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(leftMotor, OUTPUT);
+  pinMode(rightMotor, OUTPUT);
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  analogWrite(rightMotor, 110);
+  analogWrite(leftMotor, 130);
+
+}
